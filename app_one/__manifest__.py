55004f836493463cf72594f3722d041b602d2224
AYMAN KILANY <102975244+Alexaawy@users.noreply.github.com>
2025-12-21 19:55:01 +0000

    Add manifest elements

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -1,1 +1,12 @@
-
+{
+    'name': "App One",
+    'author': "Ayman Kilany K",
+    'category': "",
+    'version' : "17.0.0.1.0",
+    'depends' : ['base',
+                 ],
+    'date' : [
+        
+    ],
+    'application' : True,
+}
