a1eb2e384b568beb8798d8d70b0a6fb6d8307814
AYMAN KILANY <102975244+Alexaawy@users.noreply.github.com>
2025-12-31 21:10:02 +0200

    Add files via upload

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -1,12 +1,14 @@
-{
+var = {
     'name': "App One",
     'author': "Ayman Kilany K",
     'category': "",
-    'version' : "17.0.0.1.0",
-    'depends' : ['base',
-                 ],
-    'data' : [
-        
+    'version': "17.0.0.1.0",
+    'depends': ['base',
+                ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/property_view.xml'
     ],
-    'application' : True,
+    'application': True,
 }
